bf583e1d8d2954a08bff9c0033a3b77b3a3759b0
Sounkoun Soumare <vsoumare@yahoo.com>
2016-05-17 23:12:21 +0000

    Added an about page

--- portfolio/app/controllers/pages_controller.rb
+++ portfolio/app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
     def welcome
     end
+    def About
+    end
 end
--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/about' => 'pages#about'
   root 'pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
